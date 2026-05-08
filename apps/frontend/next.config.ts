50c2ab2c38a03a56a8d7fa7e9a83b8acde8adcb6
abrarcse29 <jucse29.398@gmail.com>
2026-05-09 04:35:33 +0600

    chore: update next.config.ts with local network IPs for multi-device dev

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['localhost:3000', '192.168.0.106:3000', '192.168.0.106'],
+  allowedDevOrigins: [
+    'localhost:3000',
+    '192.168.68.106:3000',
+    '192.168.68.106',
+    '192.168.68.101:3000',
+    '192.168.68.101',
+    '102.168.68.101:3000',
+    '102.168.68.101'
+  ],
   /* config options here */
 };
 
